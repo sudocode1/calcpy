90841b632b2ee1b0c7a99a837765165f5fe1c161
iris <52894745+sudocode1@users.noreply.github.com>
2020-02-09 19:34:10 +0000

    added more operations + time delay if using in cmd

--- calc.py
+++ calc.py
@@ -1,11 +1,12 @@
 import random
 import sys
 import os
+import time
 
 print("calc by sudocode1")
 print("https://github.com/sudocode1/calcpy")
 
-query = input("What kind of operation would you like to do? [+/-] ")
+query = input("What kind of operation would you like to do? [+/-/x//] ")
 
 if query == "+" :
     add_num1 = input("What is your first number of the operation? eg [HERE] + num ")
@@ -19,4 +20,20 @@ else :
 
         print("Your answer is ", int(sub_num1) - int(sub_num2))
     else :
-        print("Your query is invalid!")
+        if query == "x" :
+            multi_num1 = input("What is your first number of the operation? eg [HERE] x num ")
+            multi_num2 = input("What is your second number of the operation? eg num x [HERE] ")
+
+            print("Your answer is ", int(multi_num1) * int(multi_num2))
+        else :
+            if query == "/" :
+                div_num1 = input("What is your first number of the operation? eg [HERE] / num ")
+                div_num2 = input("What is your second number of the operation? eg num / [HERE] ")
+
+                print("Your answer is ", int(div_num1) / int(div_num2))
+            else :
+                print("Your query is invalid! Try running the program again.")
+
+
+
+time.sleep(7) 
